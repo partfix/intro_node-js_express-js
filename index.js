c40b46f235a2382c26b3fe42c306273ecf9e6ccb
unknown <YY235@ccs.local>
2025-02-05 05:35:10 -0800

    add new now

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const port = 3000;
 //Serve static files from the "public" folder
 app.use(express.static('public'));
 
+app.use((req, res, next)=>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+}) 
+
+app.use((err, req, next)=>{
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 //define a route for the home page
 app.get("/", (req, res)=>{
     res.send("Hello, World!");
@@ -25,3 +35,16 @@ app.post('/submit', (req, res)=>{
     const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`);
 });
+
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+  
+    items.push(newItem);
+    res.json(items);
+  });
